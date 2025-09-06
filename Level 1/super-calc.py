567288a5aeb36ca8c0c03a54d6c2d95b2956fc3f
Mehkhi <mxs6293@psu.edu>
2025-09-05 20:24:27 -0400

    printed quotient twice, and squashed bug

--- Level 1/super-calc.py
+++ Level 1/super-calc.py
@@ -15,7 +15,6 @@ print(f"You chose {a} and {b}")
 print(f"Sum: {a + b}")
 print(f"Difference: {a - b}")
 print(f"Product: {a * b}")
-print(f"Quotient: {a / b}")
 if b != 0:
     print(f"Quotient: {a / b}")
 else:
